a948771e51579b9925cb0b776138346b9a518ad3
Mathi <eshaghi.m@northeastern.edu>
2025-02-07 15:38:50 -0500

    adding lab 1

---
+++ lab 1/src/lab/pkg1/Lab1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg1;
+
+/**
+ *
+ * @author mathi
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("hello world");
+   
+    
+}
+}
